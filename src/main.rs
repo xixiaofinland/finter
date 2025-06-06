8c0bbbdbef717b0f0df00ed6820d06e59ab7d1b9
xi xiao <tdxiaoxi2@gmail.com>
2025-06-06 09:50:18 +0300

    update the v num.

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use clap::Parser;
 #[derive(Debug, Parser)]
 #[command(
     author = "Xi Xiao",
-    version = "0.1.14",
+    version = "0.1.15",
     about = "A Tmux plugin to quickly create session for folders in configured paths."
 )]
 pub struct Args {
